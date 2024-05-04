e4ee48398854baabf5a27f0c017a1e2b95319b5d
egesua <egesu06@gmail.com>
2024-05-04 16:33:20 +0300

    deleting todo function created. some additional styling.

--- src/components/TodoResult.tsx
+++ src/components/TodoResult.tsx
@@ -6,16 +6,21 @@ interface TodoResultProps {
 }
 
 const TodoResult: React.FC<TodoResultProps> = ({todos, setTodos}) => {
+
+    const deleteFunction = (id: number) => {
+        setTodos(todos.filter(todo => todo.id != id))
+    }
+
   return (
     <div>
     {
         todos && todos.map((todo, i) => (
-            <div key={i}>
+            <div style={{display: "flex", alignItems: "center", justifyContent: "space-between", width: "250px", height: "40px", margin: "20px", padding: "5px", backgroundColor: "red"}} key={i}>
                 <div>
                    {todo.todo} 
                 </div>
-                <div>
-                    <span>
+                <div style={{display: "flex", gap: "10px"}}>
+                    <span onClick={() => deleteFunction(todo.id)}>
                         Delete
                     </span>
                     <span>
