dd405b97517d6562512a2575c1abd41fc326e619
egesua <egesu06@gmail.com>
2024-05-04 16:16:26 +0300

    addfunction created. todoInput form created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import './App.css'
-import TodoInput from './components/TodoInput';
-import TodoResult from './components/TodoResult';
+import { useState } from "react";
+import "./App.css";
+import TodoInput from "./components/TodoInput";
+import TodoResult from "./components/TodoResult";
+import { ITypes } from "./types/iTypes";
 
 function App() {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<ITypes[]>([]);
+
+  const addFunction = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(todo.length > 0) {
+      setTodos((prev) => [
+        ...prev,
+        { todo: todo, id: todos.length + 1, isColor: false },
+      ]);
+      setTodo('');
+    }
+  };
 
   return (
     <div>
-      <TodoInput />
+      <TodoInput todo={todo} setTodo={setTodo} addFunction={addFunction} />
       <TodoResult />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,8 +1,14 @@
-const TodoInput = () => {
+interface TodoInputProps {
+    todo: string,
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const TodoInput:React.FC<TodoInputProps> = ({todo, setTodo}) => {
   return (
-    <div>
-      TodoInput
-    </div>
+    <form onSubmit={() => {}}>
+      <input value={todo} onChange={e => setTodo(e.target.value)} type="text" placeholder="Insert Todo here." />
+      <button type="submit">Add</button>
+    </form>
   )
 }
 
