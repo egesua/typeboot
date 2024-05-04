0b2e5ae05a9eaa88bc1ae4a1c77228020cfd1246
egesua <egesu06@gmail.com>
2024-05-04 16:18:12 +0300

    adding addFunction to TodoInput component.

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,11 +1,12 @@
 interface TodoInputProps {
     todo: string,
     setTodo: React.Dispatch<React.SetStateAction<string>>
+    addFunction: (e: React.FormEvent) => void
 }
 
-const TodoInput:React.FC<TodoInputProps> = ({todo, setTodo}) => {
+const TodoInput:React.FC<TodoInputProps> = ({todo, setTodo, addFunction}) => {
   return (
-    <form onSubmit={() => {}}>
+    <form onSubmit={e => addFunction(e)}>
       <input value={todo} onChange={e => setTodo(e.target.value)} type="text" placeholder="Insert Todo here." />
       <button type="submit">Add</button>
     </form>
